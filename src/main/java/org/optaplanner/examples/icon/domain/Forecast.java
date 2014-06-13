20b8b8076e3b99b24c88447cdcb2dff27401ca70
Lukáš Petrovický <lpetrovi@redhat.com>
2014-06-13 13:59:49 -0400

    Forecast parser

--- src/main/java/org/optaplanner/examples/icon/domain/Forecast.java
+++ src/main/java/org/optaplanner/examples/icon/domain/Forecast.java
@@ -1,57 +1,56 @@
 package org.optaplanner.examples.icon.domain;
 
-import java.math.BigDecimal;
-
 import it.unimi.dsi.fastutil.ints.Int2ObjectAVLTreeMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectRBTreeMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectSortedMap;
 
+import java.math.BigDecimal;
 
 public class Forecast {
 
     public static ForecastBuilder withPeriods(final int numPeriods) {
         return new ForecastBuilder(numPeriods);
     }
-    
+
     public static final class ForecastBuilder {
-        
+
         private final Int2ObjectSortedMap<BigDecimal> forecasts = new Int2ObjectRBTreeMap<BigDecimal>();
         private final int maxValue;
-        
+
         private ForecastBuilder(final int numPeriods) {
             this.maxValue = numPeriods;
         }
-        
+
         public ForecastBuilder addForecast(final String period, final String forecast) {
-            if (forecasts.size() == maxValue) {
+            if (this.forecasts.size() == this.maxValue) {
                 throw new IllegalStateException("Already seen the expected number of forecasts.");
             }
-            int periodId = Integer.valueOf(period);
-            if (forecasts.containsKey(periodId)) {
+            final int periodId = Integer.valueOf(period);
+            if (this.forecasts.containsKey(periodId)) {
                 throw new IllegalArgumentException("Already seen forecast for period: " + periodId);
             }
-            forecasts.put(periodId, new BigDecimal(forecast));
+            this.forecasts.put(periodId, new BigDecimal(forecast));
             return this;
         }
-        
+
         public Forecast build() {
-            return new Forecast(forecasts);
+            return new Forecast(this.forecasts);
         }
-        
+
     }
-    
+
     private final Int2ObjectSortedMap<BigDecimal> forecasts;
 
-    private Forecast(Int2ObjectSortedMap<BigDecimal> forecasts) {
+    private Forecast(final Int2ObjectSortedMap<BigDecimal> forecasts) {
         this.forecasts = new Int2ObjectAVLTreeMap<BigDecimal>(forecasts);
     }
-    
+
     public BigDecimal getForPeriod(final int period) {
-        BigDecimal forecast = forecasts.get(period);
+        final BigDecimal forecast = this.forecasts.get(period);
         if (forecast == null) {
             throw new IllegalArgumentException("Unknown forecasting period: " + period);
         }
         return forecast;
     }
-    
+
 }
--- src/main/java/org/optaplanner/examples/icon/parser/forecast/ForecastParser.java
+++ src/main/java/org/optaplanner/examples/icon/parser/forecast/ForecastParser.java
@@ -1,6 +1,25 @@
 package org.optaplanner.examples.icon.parser.forecast;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.optaplanner.examples.icon.domain.Forecast;
+import org.optaplanner.examples.icon.domain.Forecast.ForecastBuilder;
 
 public class ForecastParser {
+
+    public static Forecast parse(final File forecasts) throws IOException {
+        final List<String> lines = FileUtils.readLines(forecasts);
+        final int numOfPeriods = Integer.valueOf(lines.get(0));
+        final ForecastBuilder builder = Forecast.withPeriods(numOfPeriods);
+        for (int i = 1; i < lines.size(); i++) {
+            final String line = lines.get(i);
+            final String[] parts = line.split("\\Q \\E");
+            builder.addForecast(parts[0], parts[1]);
+        }
+        return builder.build();
+    }
 
 }
