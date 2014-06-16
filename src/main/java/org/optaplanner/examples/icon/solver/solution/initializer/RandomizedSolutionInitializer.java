05aeac7d22be80ce4629fab18f19d3b1405f87d8
Lukáš Petrovický <lpetrovi@redhat.com>
2014-06-16 15:13:32 -0400

    Completely randomize the solution initializer

--- src/main/java/org/optaplanner/examples/icon/solver/solution/initializer/RandomizedSolutionInitializer.java
+++ src/main/java/org/optaplanner/examples/icon/solver/solution/initializer/RandomizedSolutionInitializer.java
@@ -2,16 +2,18 @@ package org.optaplanner.examples.icon.solver.solution.initializer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.optaplanner.core.impl.phase.custom.CustomPhaseCommand;
 import org.optaplanner.core.impl.score.director.ScoreDirector;
 import org.optaplanner.examples.icon.domain.Machine;
-import org.optaplanner.examples.icon.domain.Period;
 import org.optaplanner.examples.icon.domain.Schedule;
 import org.optaplanner.examples.icon.domain.Task;
 
-public class FirstAvailableSolutionInitializer implements CustomPhaseCommand {
+public class RandomizedSolutionInitializer implements CustomPhaseCommand {
 
+    private static final Random RANDOM = new Random(0);
+    
     @Override
     public void changeWorkingSolution(final ScoreDirector scoreDirector) {
         this.initializeSchedule(scoreDirector, (Schedule) scoreDirector.getWorkingSolution());
@@ -20,19 +22,12 @@ public class FirstAvailableSolutionInitializer implements CustomPhaseCommand {
     private void initializeSchedule(final ScoreDirector scoreDirector, final Schedule schedule) {
         final List<Machine> machines = new ArrayList<Machine>(schedule.getMachines());
         for (final Task t : schedule.getTasks()) {
-            int id = t.getId();
-            final Machine m = machines.get(id % machines.size()); // cycle through machines
+            final Machine m = machines.get(RANDOM.nextInt(machines.size())); // cycle through machines
             scoreDirector.beforeVariableChanged(t, "executor");
             t.setExecutor(m);
             scoreDirector.afterVariableChanged(t, "executor");
             scoreDirector.beforeVariableChanged(t, "startPeriod");
-            if (id % 2 == 0) {
-                t.setStartPeriod(t.getEarliestStart()); // schedule task as early as possible
-            } else {
-                Period latestEnd = t.getLatestEnd();
-                int targetId = latestEnd.getId() - t.getDuration();
-                t.setStartPeriod(Period.get(targetId)); // schedule task as late as possible
-            }
+            t.setStartPeriod(t.getStartPeriodValueRange().createRandomIterator(RANDOM).next());
             scoreDirector.afterVariableChanged(t, "startPeriod");
         }
     }
