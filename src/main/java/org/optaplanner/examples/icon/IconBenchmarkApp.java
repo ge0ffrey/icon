523fac75d981ea8371af264eee845130f7dc2224
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2014-06-29 16:11:18 +0200

    IconBenchmarkApp

---
+++ src/main/java/org/optaplanner/examples/icon/IconBenchmarkApp.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2014 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaplanner.examples.icon;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.optaplanner.benchmark.api.PlannerBenchmark;
+import org.optaplanner.benchmark.api.PlannerBenchmarkFactory;
+import org.optaplanner.core.api.domain.solution.Solution;
+import org.optaplanner.core.api.solver.Solver;
+import org.optaplanner.core.api.solver.SolverFactory;
+import org.optaplanner.examples.icon.io.IconSolutionFileIO;
+
+public class IconBenchmarkApp {
+
+    public static void main(final String... args) throws IOException {
+        PlannerBenchmarkFactory plannerBenchmarkFactory = PlannerBenchmarkFactory.createFromXmlResource(
+                "org/optaplanner/examples/icon/benchmark/iconBenchmarkConfig.xml");
+        PlannerBenchmark plannerBenchmark = plannerBenchmarkFactory.buildPlannerBenchmark();
+        plannerBenchmark.benchmark();
+    }
+
+}
