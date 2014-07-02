de31d6d14b13ec1137968e246134ad94d2b4210e
Lukáš Petrovický <lpetrovi@redhat.com>
2014-07-01 20:41:28 -0400

    TaskAssignmentDifficultyComparator: never equals

--- src/main/java/org/optaplanner/examples/icon/domain/TaskAssignmentDifficultyComparator.java
+++ src/main/java/org/optaplanner/examples/icon/domain/TaskAssignmentDifficultyComparator.java
@@ -6,12 +6,22 @@ public class TaskAssignmentDifficultyComparator implements Comparator<TaskAssign
 
     @Override
     public int compare(final TaskAssignment o1, final TaskAssignment o2) {
-        final long d1 = o1.getTask().getDifficulty();
-        final long d2 = o2.getTask().getDifficulty();
+        final Task t1 = o1.getTask();
+        final long d1 = t1.getDifficulty();
+        final Task t2 = o2.getTask();
+        final long d2 = t2.getDifficulty();
         if (d1 > d2) {
             return 1;
         } else if (d1 == d2) {
-            return 0;
+            final int id1 = t1.getId();
+            final int id2 = t2.getId();
+            if (id1 > id2) {
+                return -1;
+            } else if (id1 == id2) {
+                return 0;
+            } else {
+                return 1;
+            }
         } else {
             return -1;
         }
